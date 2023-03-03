49dae3a8dd0bc9db38068395c263172aa50197fb
Vu Hong Sang <Sangvnnd@gmail.com>
2023-03-03 10:15:08 +0700

    update new

---
+++ src/main/java/luyenTapTrenLop/Day2_FOR_WHILE/Cau4_in_list_1000.java
@@ -0,0 +1,23 @@
+package luyenTapTrenLop.Day2_FOR_WHILE;
+
+import java.util.Scanner;
+
+public class Cau4_in_list_1000 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.printf("nhap so thu n = ");
+        double n = in.nextLong();
+        in.close();
+
+        if(n==0||n<=1){
+            System.out.printf("\n vui long nhap lai n!=0 ");
+        }
+
+        double sum=0;
+        for (int i=1;i<=n;i++){
+            sum=sum+1.0/(2.0*i);
+        }
+        System.out.printf("tong Sn= %f",sum);
+        System.exit(0);
+    }
+}
